413d745a5163c7bdc4843d05be82be485f95e211
Steve Phillips <steve@tryingtobeawesome.com>
2023-08-15 20:33:00 -0700

    Example of derived store. Not really necessary, but worth knowing about, methinks

--- src/stores.js
+++ src/stores.js
@@ -1,5 +1,15 @@
-import { writable } from 'svelte/store';
+import { writable, derived } from "svelte/store";
+import { addColor } from "./util.js";
 
 export const DEFAULT_COLOR = "#f15025";
 
 export const color = writable(DEFAULT_COLOR);
+
+export const colors = derived(
+  color,
+  $color => {
+    const allColors = addColor($color);
+    console.log("color list:", allColors);
+    return allColors;
+  }
+);
